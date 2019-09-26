ce1b0c04a1aa84a0e41784fa6cacff26f5e72c7c
Austin Zhang <austinzhang1018@gmail.com>
2019-09-26 17:26:35 -0400

    added csv skip and switch persistance and same day caching

--- autocall.py
+++ autocall.py
@@ -8,6 +8,7 @@ import email
 import csv
 import random
 import pickle
+from socket import gaierror
 
 ORG_EMAIL   = "@gmail.com"
 SMTP_SERVER = "imap.gmail.com"
@@ -21,6 +22,56 @@ def initialize_imap():
 def disconnect_imap(mail):
     mail.expunge()
     mail.close()
+
+def get_use_data():
+    num_uses = dict()
+    try:
+        with open('roster.csv') as csv_file:
+            reader = csv.DictReader(csv_file)
+            for row in reader:
+                skips = 0
+                switches = 0
+                try:
+                    if len(row['email'].strip()) == 0:
+                        raise Exception('An email is empty please check the csv file')
+                    if len(row['skips'].strip()) != 0 and int(row['skips'].strip()) != 0:
+                        skips = int(row['skips'].strip())
+                    if len(row['switches'].strip()) != 0 and int(row['switches'].strip()) != 0:
+                        switches = int(row['switches'].strip())
+                    if skips + switches != 0:
+                        num_uses[row['email'].strip().lower()] = { 'skips': skips, 'switches': switches }
+                except KeyError:
+                    raise Exception('Make sure there are columns named name, email, section, skips, and switches')
+    except FileNotFoundError:
+        raise Exception('Could not find course roster. Please place csv called roster.csv within the same folder as this program')
+    return num_uses
+
+def save_uses_to_csv(num_uses, students):
+    with open('roster.csv', 'w', newline='') as f:
+        fieldnames = ['name', 'email', 'section', 'skips', 'switches']
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writeheader()
+        for student in students:
+            written = False
+            for email in num_uses:
+                if email == student[1]:
+                    writer.writerow({
+                        'name': student[0],
+                        'email': student[1],
+                        'section': student[2],
+                        'skips': num_uses[email]['skips'],
+                        'switches': num_uses[email]['switches']
+                    })
+                    written = True
+                    break
+            if not written:
+                writer.writerow({
+                    'name': student[0],
+                    'email': student[1],
+                    'section': student[2],
+                    'skips': '',
+                    'switches': ''
+                })
 
 # returns an array with an int representing the type of email, the sender, and the subject respectively
 # 0 represents spam mail sent from outside dartmouth
@@ -130,40 +181,51 @@ def get_course_roster():
         raise Exception('Could not find course roster. Please place csv called roster.csv within the same folder as this program')
     return students
 
-# returns a dictionary of email 
-def get_use_data():
-    try:
-        with open('log.pickle', 'rb') as f:
-            return pickle.load(f)
-    except FileNotFoundError:
-        # don't have any saved logs
-        return dict()
-
 # returns a list of cached switches
 # this is needed because if someone from a later section switches into an earlier section
 # we need to make sure that they don't get added to the later section
-def get_switch_cache():
+def get_request_cache(num_uses, students, skips, switches):
     try:
-        with open('switch_cache.pickle', 'rb') as f:
-            switch_cache, cache_date = pickle.load(f)
+        with open('request_cache.pickle', 'rb') as f:
+            skip_cache, switch_cache, cache_date = pickle.load(f)
             if date.today() == cache_date:
-                return switch_cache
-        os.remove('switch_cache.pickle')
-        return list()
+                return skip_cache, switch_cache
+            else:
+                save_use_data(num_uses, students, skip_cache, switch_cache)
+        os.remove('request_cache.pickle')
+        return list(), list()
     except FileNotFoundError:
         # don't have any saved logs
-        return list()
+        return list(), list()
 
-def save_switch_cache(switches):        # Pickle the 'data' dictionary using the highest protocol available.
-    if len(switches) == 0:
+def save_request_cache(skips, switches):        # Pickle the 'data' dictionary using the highest protocol available.
+    if len(skips) == 0 and len(switches) == 0:
         return
-    with open('switch_cache.pickle', 'wb') as f:
-        pickle.dump((switches, date.today()), f, pickle.HIGHEST_PROTOCOL)
+    with open('request_cache.pickle', 'wb') as f:
+        pickle.dump((skips, switches, date.today()), f, pickle.HIGHEST_PROTOCOL)
         
-def save_use_data(data):
-    with open('log.pickle', 'wb') as f:
-        pickle.dump(data, f, pickle.HIGHEST_PROTOCOL)
-
+def save_use_data(num_uses, students, skips, switches):
+    # move the skips from our section
+    for student in students:
+        for skipper in skips:
+            if skipper[1] == student[1]:
+                try: 
+                    num_uses[student[1]]['skips'] += 1
+                except KeyError:
+                    # no logs for this student yet
+                    num_uses[student[1]] = { 'skips': 1, 'switches': 0 }
+                break
+    # move the switches from the other section
+    for student in students:
+        for switcher in switches:
+            if switcher[1] == student[1]:
+                try:
+                    num_uses[student[1]]['switches'] += 1
+                except KeyError:
+                    # no logs for this student yet
+                    num_uses[student[1]] = { 'skips': 0, 'switches': 1 }
+                break
+    save_uses_to_csv(num_uses, students)
 
 # returns a new roster without the students who have used their skips
 def apply_skips(roster, skips, num_uses, section):
@@ -188,13 +250,11 @@ def apply_skips(roster, skips, num_uses, section):
                 if num_uses[student[1]]['skips'] >= 5:
                     print(student[0] + ' attempted to use a skip but they have used all 5')
                 else:
-                    num_uses[student[1]]['skips'] += 1
-                    print(student[0] + ' has now used ' + str(num_uses[student[1]]['skips']) + ' skips')
+                    print(student[0] + ' has now used ' + str(num_uses[student[1]]['skips'] + 1) + ' skips')
                     skipped = True
             except KeyError:
                 # this student hasn't used any skips or switches yet
                 print(student[0] + ' has now used 1 skip.')
-                num_uses[student[1]] = { 'skips': 1, 'switches': 0 }
                 skipped = True
             # break so we don't process multiple skips for the same student
             break
@@ -212,15 +272,18 @@ def get_sections(roster):
 
 def prompt_sections(sections):
     while True:
-        prompt = 'Please select one of the following sections by typing its name:'
+        prompt = 'Please type both or select one section by typing its name:'
         for section in sorted(sections):
             prompt += ' ' + section
         print(prompt)
         selected_section = input().lower().strip()
+
+        if selected_section == 'both':
+            return tuple(sorted(sections))
         
         if selected_section in sections:
             print('Section ' + selected_section + ' selected')
-            return selected_section
+            return selected_section, False
 
 # ASSUMES THERE'S ONLY TWO SECTIONS, ONLY PART OF CODE THAT MAKES THIS ASSUMPTION
 # filters all of the students who should be in our section
@@ -239,12 +302,9 @@ def apply_switches(roster, switches, num_uses, section):
             if student[2] == section:
                 break
             try:
-                num_uses[student[1]]['switches'] += 1
-                switches_used = num_uses[student[1]]['switches']
-                print(student[0] + ' has now switched ' + str(switches_used) + ' times')
+                print(student[0] + ' has now switched ' + str(num_uses[student[1]]['switches'] + 1) + ' times')
             except KeyError: # this occurs if the student hasn't switched or skipped
                 print(student[0] + ' has now switched 1 time')
-                num_uses[student[1]] = { 'skips': 0, 'switches': 1 }
             break
         if (student[2] == section and not switched) or (student[2] != section and switched):
             new_roster.append(student)
@@ -252,24 +312,18 @@ def apply_switches(roster, switches, num_uses, section):
         
 def prompt_action():
     print()
-    print('The cold call list has been generated.')
+    print('The cold call list has been downloaded.')
     print()
-    print('Type save to download')
-    print('Type reset to download and log requests')
-    print('Type return to download, log request, and select another section')
-    print('Type exit to quit without downloading')
+    print('Type again to download and select another section')
+    print('Type exit to quit')
     while True:
         command = input().lower().strip()
-        if command == 'save':
-            return 0
-        elif command == 'reset':
-            return 1
-        elif command == 'return':
-            return 2
+        if command == 'again':
+            return
         elif command == 'exit':
             exit()
         else:
-            print('Unrecognized command. Valid commands are save, reset, and return.')
+            print('Unrecognized command. Valid commands are save, reset, return, and exit.')
 
 def write_list(call_list, section):
     with open('call_list_' + section + '.csv', 'w', newline='') as f:
@@ -292,38 +346,47 @@ def move_processed_emails(mail, students, skips, switches, section):
                 move_email(mail, switcher[0])
                 break
 
-def combine_switches(switches, switch_cache):
+def combine_cache(request, request_cache):
     combined = set()
-    combined.update(switches)
-    combined.update(switch_cache)
+    combined.update(request)
+    combined.update(request_cache)
     return combined
 
 
-def main():
+def main(given_section=None):
     # login to email
     mail = initialize_imap()
     skips, switches = read_emails(mail)
-    switches = combine_switches(switches, get_switch_cache())
     students = get_course_roster()
     call_list = list.copy(students)
     num_uses = get_use_data()
-    section = prompt_sections(get_sections(call_list))
+    skip_cache, switch_cache = get_request_cache(num_uses, students, skips, switches)
+    skips = combine_cache(skips, skip_cache)
+    switches = combine_cache(switches, switch_cache)
+    section = given_section
+    both = False
+    if not section:
+        section, both = prompt_sections(get_sections(call_list))
     call_list = apply_skips(call_list, skips, num_uses, section)
     call_list = apply_switches(call_list, switches, num_uses, section)
     # call list should now only be students who are valid cold call candidates
     random.shuffle(call_list)
-    action = prompt_action()
     # write out csv regardless of action
     write_list(call_list, section)
-    if action == 1 or action == 2:
-        save_use_data(num_uses)
-        save_switch_cache(switches)
-        move_processed_emails(mail, students, skips, switches, section)
-        if action == 2:
-            return True
+    print('The cold call list has been downloaded for section ' + section + '.')
+    save_request_cache(skips, switches)
+    move_processed_emails(mail, students, skips, switches, section)
+    return both
+    
 
 
 if __name__ == '__main__':
-    while True:
-        if not main():
-            exit()
+    try:
+        section = None
+        while True:
+            section = main(section)
+            if not section:
+                exit()
+                
+    except gaierror:
+        print('Cannot connect to internet. Please check your connection and try again.')
